0960aa0947951d2cddab74a65e9ac20dcaaa7dff
eggsonafryingpan <eggsonafryingpan@gmail.com>
2026-01-24 16:29:01 -0500

    sdfsdfs

--- RoomFlowerDemo/script.js
+++ RoomFlowerDemo/script.js
@@ -1,9 +1,14 @@
 const world = document.getElementById("world");
 console.log(world);
+const width = 1400;
+const height = 788;
 world.style.backgroundImage = "url('imgs/room.png')";
-
+world.style.width = `${width * dpr / dpr}px`;
+world.style.height = `${height * dpr / dpr}px`;
 document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
     console.log(mouseX, mouseY);
 })
+
+const dpr = window.devicePixelRatio || 1;
