54a6d7f35da10db8f253e67b0ed70f9e8fcf9416
eggsonafryingpan <eggsonafryingpan@gmail.com>
2026-01-24 16:02:45 -0500

    sdfsdf

--- RoomFlowerDemo/script.js
+++ RoomFlowerDemo/script.js
@@ -2,11 +2,8 @@ const world = document.getElementById("world");
 console.log(world);
 world.style.backgroundImage = "url('imgs/room.png')";
 
-const gameWidth = 800;
-const gameHeight = 450;
-console.log(innerHeight + " " + innerWidth);
-const screenWidth = window.innerWidth;
-const screenHeight = window.innerHeight;
-
-
-const Xscale = Math.floor(gameWidth / screenWidth);
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    console.log(mouseX, mouseY);
+})
