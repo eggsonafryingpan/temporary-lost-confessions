7f094ca3160443e32e2df2053b017a3f68f2969f
eggsonafryingpan <eggsonafryingpan@gmail.com>
2026-01-24 15:52:46 -0500

    console log

--- RoomFlowerDemo/script.js
+++ RoomFlowerDemo/script.js
@@ -4,7 +4,7 @@ world.style.backgroundImage = "url('imgs/room.png')";
 
 const gameWidth = 800;
 const gameHeight = 450;
-
+console.log(innerHeight + " " + innerWidth);
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
 
